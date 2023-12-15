d58923aba6567f7540cd07dec997bf2f191c091c
BagusHartiansyah@G18 <53298436+BagusHartiansyah-2G18@users.noreply.github.com>
2023-12-15 13:59:42 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,9 +6,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from babel.numbers import format_currency
-sns.set(style='dark')
 import numpy as np
  
 
