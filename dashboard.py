3b921ffc812db992ed4c07a2ac4fa9e174d2969e
BagusHartiansyah@G18 <53298436+BagusHartiansyah-2G18@users.noreply.github.com>
2023-12-15 14:24:03 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -25,7 +25,8 @@ with st.sidebar:
 
 
 # read data 
-allData = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vRrTVLo9EUE9eGc6sAw-WsLIDDUaAVBVmGvaKQjTz7696busOoh32mXD0RDIVs_TqJnVwJBEoT-qbzi/pub?output=xlsx")
+# allData = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vRrTVLo9EUE9eGc6sAw-WsLIDDUaAVBVmGvaKQjTz7696busOoh32mXD0RDIVs_TqJnVwJBEoT-qbzi/pub?output=xlsx")
+allData =[]
 
 # get daftar Desa dan jumlah nya 
 # desaC = allData.groupby(by=['desa']).desa.count() 
